0fd312c9e92c2ef014bd026fc059d90d2a34419a
Jordan Owens <jordanowens1125@gmail.com>
2024-11-25 22:18:58 -0600

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import localFont from "next/font/local";
-import "./globals.css";
+import "./globals.scss";
+import Footer from "./components/footer";
+import NavBar from "./components/nav";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -21,7 +23,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <NavBar />
         {children}
+        <Footer />
       </body>
     </html>
   );
